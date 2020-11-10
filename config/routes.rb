b250849f648ae7d3c819eeda58595676f7d78554
Melodie Souksone <msouksone@gmail.com>
2020-11-10 16:03:22 +0100

    exo pas terminé

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: "tasks#list"
+  get 'tasks/new', to: "tasks#new", as: :task_new
+
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  get "tasks/:id", to: "tasks#show", as: :task
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy"
+
 end
